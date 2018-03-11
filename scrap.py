63428c065954567cfcc455bd79ece34190af4f2f
Radek Matěj <radek@nikdo.cz>
2018-03-11 15:13:16 +0100

    Fix python3 compatibility & replace urllib2 by requests

--- scrap.py
+++ scrap.py
@@ -1,6 +1,5 @@
-import urllib2
-import requests
 import re
+import requests
 import datetime
 from bs4 import BeautifulSoup
 
@@ -9,14 +8,15 @@ page = requests.get(DOMAIN + "hp.sqw?k=192")
 soupList = BeautifulSoup(page.content, "html.parser", from_encoding="windows-1250")
 timestamp = datetime.datetime.now().strftime("%d.%m.%Y")
 
-print soupList.title.string
+print(soupList.title.string)
 
 #print header
 print("\"poradi\";\"casova_znamka\";\"poslanec_odkaz\";\"poslanec_jmeno\";\"poslanec_adresa\";\"asistent_jmeno\"")
 
 i = 1
 for memberLink in soupList.find_all("span", "name"):
-    soupMember = BeautifulSoup(urllib2.urlopen(DOMAIN + memberLink.a['href']), "html.parser", from_encoding="windows-1250")
+    detailPage = requests.get(DOMAIN + memberLink.a['href'])
+    soupMember = BeautifulSoup(detailPage.content, "html.parser", from_encoding="windows-1250")
     for memberAssistantCollection in soupMember.find_all("ul", "assistants"):
         for memberAssistant in memberAssistantCollection.find_all("strong"):
             # Adresa
@@ -26,7 +26,7 @@ for memberLink in soupList.find_all("span", "name"):
             #print type(addressMember)
             if (addressMember != "None"):
                 addressReg = re.search('^<address.*?>([^<]*)', addressMember)
-                address = str(addressReg.group(1)).decode("UTF-8")
+                address = str(addressReg.group(1))
 
             # Record
             print("\"" + str(i) + "\";\"" + timestamp + "\";\"" + DOMAIN + memberLink.a['href'] + "\";\"" + soupMember.h1.string + "\";\"" + address + "\";\"" + memberAssistant.string + "\"")
