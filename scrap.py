751db932eeff8ff47d5d258184b415354c1a6dd0
Radek Matěj <radek@nikdo.cz>
2018-03-11 16:59:01 +0100

    Replace non-breaking spaces

--- scrap.py
+++ scrap.py
@@ -8,17 +8,20 @@ listPage = requests.get(DOMAIN + "hp.sqw?k=192")
 listSoup = BeautifulSoup(listPage.content, "html.parser", from_encoding="windows-1250")
 timestamp = datetime.datetime.now().strftime("%d.%m.%Y")
 
+def replaceNbsp (string):
+    return string.replace(u"\xa0", " ")
+
 for memberLink in listSoup.select(".person-list .name a"):
     memberPage = requests.get(DOMAIN + memberLink["href"])
     memberSoup = BeautifulSoup(memberPage.content, "html.parser", from_encoding="windows-1250")
     member = {
-        "name": memberSoup.h1.string,
-        "address": memberSoup.address.get_text(),
+        "name": replaceNbsp(memberSoup.h1.string),
+        "address": replaceNbsp(memberSoup.address.get_text()),
         "assistants": [],
         "url": memberLink["href"],
         "timestamp": timestamp
     }
     for assistant in memberSoup.select("ul.assistants li strong"):
-        member["assistants"].append(assistant.string)
+        member["assistants"].append(replaceNbsp(assistant.string))
 
     pprint.pprint(member)
