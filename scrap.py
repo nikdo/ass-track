7ffb686533c932048492e6a924785da879ef7de2
Radek Matěj <radek@nikdo.cz>
2018-03-11 17:40:02 +0100

    Extract parseMember function

--- scrap.py
+++ scrap.py
@@ -4,19 +4,14 @@ import pprint
 import re
 from bs4 import BeautifulSoup
 
-DOMAIN = "http://www.psp.cz/sqw/"
-listPage = requests.get(DOMAIN + "hp.sqw?k=192")
-listSoup = BeautifulSoup(listPage.content, "html.parser", from_encoding="windows-1250")
-timestamp = datetime.datetime.now().strftime("%d.%m.%Y")
-
 def replaceNbsp (string):
     return string.replace(u"\xa0", " ")
 
-for memberLink in listSoup.select(".person-list .name a"):
-    memberPage = requests.get(DOMAIN + memberLink["href"])
+def parseMember (url):
+    memberPage = requests.get(url)
     memberSoup = BeautifulSoup(memberPage.content, "html.parser", from_encoding="windows-1250")
     member = {
-        "id": re.search("id=(.*)", memberLink["href"])[1],
+        "id": re.search("id=(.*)", url)[1],
         "name": replaceNbsp(memberSoup.h1.string),
         "address": replaceNbsp(memberSoup.address.get_text()),
         "assistants": [],
@@ -24,5 +19,13 @@ for memberLink in listSoup.select(".person-list .name a"):
     }
     for assistant in memberSoup.select("ul.assistants li strong"):
         member["assistants"].append(replaceNbsp(assistant.string))
+    return member
 
+DOMAIN = "http://www.psp.cz/sqw/"
+listPage = requests.get(DOMAIN + "hp.sqw?k=192")
+listSoup = BeautifulSoup(listPage.content, "html.parser", from_encoding="windows-1250")
+timestamp = datetime.datetime.now().strftime("%d.%m.%Y")
+
+for memberLink in listSoup.select(".person-list .name a"):
+    member = parseMember(DOMAIN + memberLink["href"])
     pprint.pprint(member)
