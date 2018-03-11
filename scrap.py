0ac3d9d158cd8830da3803386d28f0ad6cead909
Radek Matěj <radek@nikdo.cz>
2018-03-11 18:15:55 +0100

    Create snapshot & set up logging

--- scrap.py
+++ scrap.py
@@ -1,13 +1,16 @@
-import requests
 import datetime
+import logging
 import pprint
 import re
+import requests
 from bs4 import BeautifulSoup
+
+logging.getLogger().setLevel(logging.INFO)
 
 def replaceNbsp (string):
     return string.replace(u"\xa0", " ")
 
-def parseMember (url):
+def scrapMember (url):
     memberPage = requests.get(url)
     memberSoup = BeautifulSoup(memberPage.content, "html.parser", from_encoding="windows-1250")
     member = {
@@ -15,17 +18,23 @@ def parseMember (url):
         "name": replaceNbsp(memberSoup.h1.string),
         "address": replaceNbsp(memberSoup.address.get_text()),
         "assistants": [],
-        "timestamp": timestamp
     }
     for assistant in memberSoup.select("ul.assistants li strong"):
         member["assistants"].append(replaceNbsp(assistant.string))
+    logging.info("Scrapped member " + member["name"] + " from " + url)
     return member
 
-DOMAIN = "http://www.psp.cz/sqw/"
-listPage = requests.get(DOMAIN + "hp.sqw?k=192")
-listSoup = BeautifulSoup(listPage.content, "html.parser", from_encoding="windows-1250")
-timestamp = datetime.datetime.now().strftime("%d.%m.%Y")
+def scrapSnapshot ():
+    DOMAIN = "http://www.psp.cz/sqw/"
+    listPage = requests.get(DOMAIN + "hp.sqw?k=192")
+    listSoup = BeautifulSoup(listPage.content, "html.parser", from_encoding="windows-1250")
+    snapshot = {
+        "timestamp": datetime.datetime.now().strftime("%d.%m.%Y"),
+        "members": [],
+    }
+    for memberLink in listSoup.select(".person-list .name a"):
+        snapshot["members"].append(scrapMember(DOMAIN + memberLink["href"]))
+    logging.info("Scrapped " + str(len(snapshot["members"])) + " members.")
+    return snapshot
 
-for memberLink in listSoup.select(".person-list .name a"):
-    member = parseMember(DOMAIN + memberLink["href"])
-    pprint.pprint(member)
+pprint.pprint(scrapSnapshot())
