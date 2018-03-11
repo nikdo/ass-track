8e17ed1d6dff4748520634ff909c22892048a570
Radek Matěj <radek@nikdo.cz>
2018-03-11 16:52:15 +0100

    Clean up script

--- scrap.py
+++ scrap.py
@@ -1,33 +1,24 @@
-import re
 import requests
 import datetime
+import pprint
 from bs4 import BeautifulSoup
 
 DOMAIN = "http://www.psp.cz/sqw/"
-page = requests.get(DOMAIN + "hp.sqw?k=192")
-soupList = BeautifulSoup(page.content, "html.parser", from_encoding="windows-1250")
+listPage = requests.get(DOMAIN + "hp.sqw?k=192")
+listSoup = BeautifulSoup(listPage.content, "html.parser", from_encoding="windows-1250")
 timestamp = datetime.datetime.now().strftime("%d.%m.%Y")
 
-print(soupList.title.string)
+for memberLink in listSoup.select(".person-list .name a"):
+    memberPage = requests.get(DOMAIN + memberLink["href"])
+    memberSoup = BeautifulSoup(memberPage.content, "html.parser", from_encoding="windows-1250")
+    member = {
+        "name": memberSoup.h1.string,
+        "address": memberSoup.address.get_text(),
+        "assistants": [],
+        "url": memberLink["href"],
+        "timestamp": timestamp
+    }
+    for assistant in memberSoup.select("ul.assistants li strong"):
+        member["assistants"].append(assistant.string)
 
-#print header
-print("\"poradi\";\"casova_znamka\";\"poslanec_odkaz\";\"poslanec_jmeno\";\"poslanec_adresa\";\"asistent_jmeno\"")
-
-i = 1
-for memberLink in soupList.find_all("span", "name"):
-    detailPage = requests.get(DOMAIN + memberLink.a['href'])
-    soupMember = BeautifulSoup(detailPage.content, "html.parser", from_encoding="windows-1250")
-    for memberAssistantCollection in soupMember.find_all("ul", "assistants"):
-        for memberAssistant in memberAssistantCollection.find_all("strong"):
-            # Adresa
-            addressMember = str(soupMember.address)
-            #print addressMember
-            address = "Neuvedena"
-            #print type(addressMember)
-            if (addressMember != "None"):
-                addressReg = re.search('^<address.*?>([^<]*)', addressMember)
-                address = str(addressReg.group(1))
-
-            # Record
-            print("\"" + str(i) + "\";\"" + timestamp + "\";\"" + DOMAIN + memberLink.a['href'] + "\";\"" + soupMember.h1.string + "\";\"" + address + "\";\"" + memberAssistant.string + "\"")
-    i += 1
+    pprint.pprint(member)
