fc6b88b72c7bb95e94bb66215e10ec6bb9a7ed06
Radek Matěj <radek@nikdo.cz>
2018-04-18 09:26:11 +0200

    Move database name to connection string

--- db.py
+++ db.py
@@ -6,6 +6,6 @@ import config
 def save_snapshot (snapshot):
     logging.info("Connecting to database on " + config.DB_CONNECTION_STRING)
     client = MongoClient(config.DB_CONNECTION_STRING)
-    db=client.ass_track
+    db = client.get_default_database()
     db.snapshots.insert_one(snapshot)
     logging.info("Snapshot persisted in database.")
