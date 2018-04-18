6af42f96f22f4df2583182decf2abbf40c8d9ec6
Radek Matěj <radek@nikdo.cz>
2018-04-18 09:36:45 +0200

    Set production logging level

--- config.py
+++ config.py
@@ -2,8 +2,9 @@ import logging
 import os
 from dotenv import load_dotenv, find_dotenv
 
-logging.getLogger().setLevel(logging.INFO)
 load_dotenv(find_dotenv(), verbose=True)
 
 IS_PRODUCTION = os.environ.get("PYTHON_ENV") == "production"
 DB_CONNECTION_STRING = os.environ.get("DB_CONNECTION_STRING")
+
+logging.getLogger().setLevel(logging.INFO if IS_PRODUCTION else logging.DEBUG)
--- db.py
+++ db.py
@@ -4,8 +4,8 @@ from pymongo import MongoClient
 import config
 
 def save_snapshot (snapshot):
-    logging.info("Connecting to database on " + config.DB_CONNECTION_STRING)
+    logging.debug("Connecting to database on " + config.DB_CONNECTION_STRING)
     client = MongoClient(config.DB_CONNECTION_STRING)
     db = client.get_default_database()
     db.snapshots.insert_one(snapshot)
-    logging.info("Snapshot persisted in database.")
+    logging.debug("Snapshot persisted in database.")
