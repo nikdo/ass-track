ab323177656d95a10ddd3b64317974807f044fdd
Radek Matěj <radek@nikdo.cz>
2018-04-26 01:27:27 +0200

    Format snapshot date & time

--- web.py
+++ web.py
@@ -13,5 +13,10 @@ def home():
     last_snapshot = db.get_last_snapshot()
     return render_template('home.html', snapshot = last_snapshot)
 
+def format_datetime(value):
+    return value.strftime('%-d.%-m.%Y %-H:%M')
+
+app.jinja_env.filters['datetime'] = format_datetime
+
 if __name__ == '__main__':
     app.run()
